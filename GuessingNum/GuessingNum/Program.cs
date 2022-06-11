b467e21ab4103683342d0c9cdce5d80cb657158f
omidsn <omid.snnnn@gmail.com>
2022-06-11 15:53:44 +0430

    first commit

---
+++ GuessingNum/GuessingNum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int WinNum = r.Next(0, 100);
+
+            bool Win = false;
+
+            do
+            {
+                Console.WriteLine("Guess A Number Between 0 To 100");
+
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > WinNum)
+                {
+                    Console.WriteLine("To High !! Guess Lower ...");
+                }
+                else if (i < WinNum)
+                {
+                    Console.WriteLine("To Low !! Guess Higher ...");
+                }
+                else if (i == WinNum)
+                {
+                    Console.WriteLine("You Win !!!");
+                    Win = true;
+                }
+                Console.WriteLine();
+
+            } while (Win == false);
+
+            Console.WriteLine("Thanks For Playing The Game :)");
+            Console.WriteLine("Press Any Key For Quit");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            Action<int> printodd = x =>
+            {
+                var square = x * x;
+                var cube = x * x * x;
+                Console.WriteLine($"{x} : {square} , {cube}");
+            };
+
+            Func<int, bool> oddcheck = x => x%2!=0;
+
+            var oddnumbers = numbers.Where(oddcheck).ToList();
+
+            oddnumbers.ForEach(printodd);
+
+            Console.ReadLine();
+
+        }
+    }
+}
